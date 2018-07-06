406513ed585909986f3b6d628a0daee8b0a82311
Robert van Bregt <robert@robertvanbregt.nl>
2018-07-06 20:33:13 +0200

    Initial version of a working sensor

--- custom_components/sensor/iex.py
+++ custom_components/sensor/iex.py
@@ -1,23 +1,33 @@
-from homeassistant.const import TEMP_CELSIUS
+"""
+Support for iextrading.com stock prices.
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.iex/
+"""
+import logging
+
 from homeassistant.helpers.entity import Entity
 
+REQUIREMENTS = ['iexfinance==0.3.3']
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
-    add_devices([ExampleSensor()])
+    add_devices([IEXSensor()])
 
 
-class ExampleSensor(Entity):
+class IEXSensor(Entity):
     """Representation of a Sensor."""
 
     def __init__(self):
         """Initialize the sensor."""
+
+        from iexfinance import Stock
+
         self._state = None
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Example Temperature'
+        return 'IEX'
 
     @property
     def state(self):
@@ -27,11 +37,11 @@ class ExampleSensor(Entity):
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement."""
-        return TEMP_CELSIUS
+        return 'IEX'
 
     def update(self):
         """Fetch new state data for the sensor.
 
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._state = 23
+        self._state = 0
