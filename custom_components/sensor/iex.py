fae8ebef65d8b22d4af610229e98b0947811a313
Robert van Bregt <robert@robertvanbregt.nl>
2018-07-07 20:14:21 +0200

    Initial release of working sensor

--- custom_components/sensor/iex.py
+++ custom_components/sensor/iex.py
@@ -9,17 +9,24 @@ import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
-    ATTR_ATTRIBUTION, CONF_CURRENCY, CONF_NAME)
+    ATTR_ATTRIBUTION, CONF_CURRENCY, CONF_FORCE_UPDATE, 
+    CONF_NAME)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
-
 
 REQUIREMENTS = ['iexfinance==0.3.3']
 
 _LOGGER = logging.getLogger(__name__)
 
+ATTR_CLOSE = 'close'
+ATTR_COMPANY = 'company'
+ATTR_EXCHANGE = 'exchange'
+ATTR_HIGH = 'high'
+ATTR_LOW = 'low'
+ATTR_SYMBOL = 'symbol'
+ATTR_VOLUME = 'volume'
+
 CONF_ATTRIBUTION = "Data provided for free by IEX. View IEX’s Terms of Use. (https://iextrading.com/api-exhibit-a/)"
-CONF_CURRENCY = 'currency'
 CONF_SYMBOL = 'symbol'
 CONF_SYMBOLS = 'symbols'
 
@@ -59,23 +66,24 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
+    from iexfinance import get_available_symbols
     from iexfinance import Stock
+
+    avail = get_available_symbols()
 
     symbols = config.get(CONF_SYMBOLS, [])
 
     dev = []
 
     for symbol in symbols:
-        try:
-            _LOGGER.debug("Configuring stock for symbol: %s",
-                          symbol[CONF_SYMBOL])
-            stock = Stock(symbol[CONF_SYMBOL])
-        except ValueError as error:
-            _LOGGER.error(
-                "Unknown symbol '%s'", symbol)
-            _LOGGER.debug(str(error))
-        dev.append(IEXSensor(stock, symbol))
 
+        symbol_found = next((item for item in avail if item["symbol"] == symbol[CONF_SYMBOL]), None)
+        if symbol_found is not None:
+            stock = Stock(symbol[CONF_SYMBOL])
+            dev.append(IEXSensor(stock, symbol))
+            _LOGGER.debug("Device created for symbol %s", symbol[CONF_SYMBOL])
+        else:
+            _LOGGER.warn("No sensor created for unsupported symbol %s", symbol[CONF_SYMBOL])
 
     add_devices(dev, True)
     _LOGGER.debug("Setup completed")
@@ -100,7 +108,23 @@ class IEXSensor(Entity):
     @property
     def state(self):
         """Return the state of the sensor."""
-        return self._state
+        if self.values is not None:
+            return self.values['latestPrice']
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        if self.values is not None:
+            return {
+                ATTR_CLOSE: self.values['close'],
+                ATTR_HIGH: self.values['high'],
+                ATTR_LOW: self.values['low'],
+                ATTR_VOLUME: self.values['latestVolume'],
+                ATTR_SYMBOL: self.values['symbol'],
+                ATTR_COMPANY: self.values['companyName'],
+                ATTR_EXCHANGE: self.values['primaryExchange'],
+                ATTR_ATTRIBUTION: CONF_ATTRIBUTION,
+            }
 
     @property
     def unit_of_measurement(self):
@@ -113,9 +137,14 @@ class IEXSensor(Entity):
         return self._icon
 
     def update(self):
-        """Fetch new state data for the sensor.
+        """Get the latest data and updates the states."""
+        _LOGGER.debug("Requesting new data for symbol %s", self._symbol)
 
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self._state = 0
-        _LOGGER.debug("Device updated")
+        try:
+            self.values = self._stock.get_quote()
+            _LOGGER.debug("Received new values for symbol %s", self._symbol)
+        except ValueError as error:
+            _LOGGER.error(
+                "Unknown symbol '%s'", self._symbol)
+            _LOGGER.debug('Error: ' + str(error))
+
